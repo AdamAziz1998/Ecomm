b5620dcbc646cc7758a2677eab397a178bb3efc2
AdamAziz1998 <101112712+AdamAziz1998@users.noreply.github.com>
2024-01-07 17:08:16 +0000

    user microservice init

---
+++ user/src/test/java/com/azizONeill/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizONeill.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
